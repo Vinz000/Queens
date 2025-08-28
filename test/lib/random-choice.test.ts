5f2fe72eae5c81e752225167e106963353027dbf
Vinz000 <jvpandac0@gmail.com>
2025-08-29 00:37:02 +0100

    test: added tests for random choice

---
+++ test/lib/random-choice.test.ts
@@ -0,0 +1,14 @@
+import { randomChoice } from "@/lib/random-choice";
+import { describe, expect, it } from "vitest";
+
+describe("randomChoice", () => {
+  it("should return an element from an array", () => {
+    const array = [1, 2, 3, 4];
+    const value = randomChoice(array);
+    expect(array).toContain(value);
+  });
+
+  it("should throw an error when passing empty array", () => {
+    expect(() => randomChoice([])).toThrow();
+  });
+});
