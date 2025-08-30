8279bdbf0837ee92cc5767dc79a9e57baa0d52e5
Vinz000 <jvpandac0@gmail.com>
2025-08-30 02:43:43 +0100

    chore: update import for script

--- scripts/generate-fundamental-queens.ts
+++ scripts/generate-fundamental-queens.ts
@@ -2,7 +2,7 @@
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-import { generateFundamentalQueens } from "@/lib/generate-fundamental-queens";
+import { generateFundamentalQueens } from "@/core/generate-fundamental-queens";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
